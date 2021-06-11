dc753ed92b21b628b310d95f958c3da620b0902e
cpuCode <923992029@qq.com>
2021-06-11 21:21:48 +0800

    整合 nacos

--- mall-coupon/src/main/java/com/cpucode/mall/coupon/MallCouponApplication.java
+++ mall-coupon/src/main/java/com/cpucode/mall/coupon/MallCouponApplication.java
@@ -3,7 +3,9 @@ package com.cpucode.mall.coupon;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @MapperScan("com.cpucode.mall.coupon.dao")
 @SpringBootApplication
 public class MallCouponApplication {
